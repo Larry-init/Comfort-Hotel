e1bb13d84917a7f33c86b2c747f6b045315d6f1b
Olanrewaju Adeniji <lanreadeniji77@gmail.com>
2020-11-09 21:13:37 +0100

    new change

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ function initMap(){
 }
 
 //For hamburger menu
+let times = document.querySelector('.times'),
+    bar = document.querySelector('.bar'),
+    timesBtn = document.querySelector('.timesBtn'),
+    barBtn = document.querySelector('.barBtn');
+    
+barBtn.addEventListener('click', function(){
+    if(timesBtn.classList.contains('timesBtn')){
+        times.classList.toggle('timesOut');
+    }
+})
+timesBtn.addEventListener('click', function(){
+    if(timesBtn.classList.contains('timesBtn')){
+        times.classList.toggle('timesOut');
+    }
+})
