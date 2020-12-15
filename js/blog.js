f46e05da72ae43421c014172c2ff6551ff9586d3
Olanrewaju Adeniji <lanreadeniji77@gmail.com>
2020-12-15 14:17:44 +0100

    new change

--- js/blog.js
+++ js/blog.js
@@ -14,3 +14,14 @@ timesBtn.addEventListener('click', function(){
         times.classList.toggle('timesOut');
     }
 })
+
+//Increasing and decreasing the blog post
+
+let more = document.querySelector('.more'),
+    decrease = document.querySelector('.decrease');
+
+more.addEventListener("click", function (){
+    if (more.classList.contains('more')){
+        decrease.classList.toggle('increase')
+    }
+})
